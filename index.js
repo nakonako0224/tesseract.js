7d0ce19abc12f51c22aa904ddf9f4b1f75ae7675
KM04092 <31394849+KM04092@users.noreply.github.com>
2017-08-28 15:44:37 +0900

    sample2
    langage_select

--- index.js
+++
@@ -1,8 +0,0 @@
-Tesseract.recognize("./test.png",{lang:"jpn"}).then(function(result){
-    const a = document.querySelector("#test");
-    a.innerHTML = result.html;
-})
-Tesseract.recognize("./test2.png").then(function(result){
-    const a = document.querySelector("#test2");
-    a.innerHTML = result.html;
-})
