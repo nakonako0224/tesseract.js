4b214be3ccc90fd530222a3d98a9acdf5da88a26
KM04092 <31394849+KM04092@users.noreply.github.com>
2017-08-30 12:54:50 +0900

    js_modification
    js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function read_in() {
 	var reader = new FileReader();
 	reader.onload = function (e) {
-		//選択された画像をimg要素として表示する
+		//選択された画像をimg要素として表示
 		read_image.src = reader.result
-		//画像認識を行う
+		//画像認識関数
 		recognize_image();
 	}
 	// 画像ファイルをdata URLとして読み込むように指示
@@ -14,38 +14,36 @@ function read_in() {
 
 //画像認識
 function recognize_image() {
-  //結果出力先の要素を取得する。
-  var txt_out = document.getElementById("text_of_read_image");
-  //複数回、連続して実行するときのために、最初に中身を捨てる。
-  txt_out.innerHTML = "";
-  //指定された言語のコードを取得する。
-  var lang_list = document.getElementById("lang_options");
-  var selected_lang = lang_list.options[lang_list.selectedIndex].value;
-  console.log(selected_lang + "が選択されました。");
-  
-  var startTime = new Date();
-  var starthms = startTime.getHours() + ":" + startTime.getMinutes() + "." + startTime.getSeconds();
-  document.getElementById("msg").textContent = "[処理開始" + starthms + "]\n";
+	//結果出力先の要素を取得
+	var txt_out = document.getElementById("text_of_read_image");
+	//複数回、連続して実行するときのために、最初に中身を捨てる
+	txt_out.innerHTML = "";
+	//指定された言語のコードを取得
+	var lang_list = document.getElementById("lang_options");
+	var selected_lang = lang_list.options[lang_list.selectedIndex].value;
+	console.log(selected_lang + "が選択されました。");
 
-  //画像認識
-  Tesseract.recognize(
-    document.getElementById("read_image").src,
-    { 
-      lang: selected_lang
-    })
-  .progress(progressUpdate)
-  .catch(function(e) {
-    document.getElementById("msg").textContent += "[ERROR: " + e + "]\n";
-    console.log("ERROR: " + e);
-  })
-  .then(function(result) {
-    txt_out.innerHTML = result.text;
-  })
-  .finally(function(r) {
-    var endTime = new Date();
-    var endhms = endTime.getHours() + ":" + endTime.getMinutes() + "." + endTime.getSeconds();
-    document.getElementById("msg").textContent += "[処理終了" + endhms + "]\n";
-  });
+	var startTime = new Date();
+	var starthms = startTime.getHours() + ":" + startTime.getMinutes() + "." + startTime.getSeconds();
+	document.getElementById("msg").textContent = "[処理開始" + starthms + "]\n";
+
+	//画像認識
+	Tesseract.recognize(document.getElementById("read_image").src, { 
+			lang: selected_lang
+		})
+		.progress(progressUpdate)
+		.catch(function(e) {
+			document.getElementById("msg").textContent += "[ERROR: " + e + "]\n";
+			console.log("ERROR: " + e);
+		})
+		.then(function(result) {
+	  		txt_out.innerHTML = result.text;
+		})
+		.finally(function(r) {
+			var endTime = new Date();
+			var endhms = endTime.getHours() + ":" + endTime.getMinutes() + "." + endTime.getSeconds();
+			document.getElementById("msg").textContent += "[処理終了" + endhms + "]\n";
+		});
 }
 
 //進捗管理
